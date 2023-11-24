eecddcc31eaee7d86d2470830e5efe589412ed1b
lingongbing <lingongbing>
2023-11-24 16:54:38 +0800

    * 初始化配置

---
+++ .prettierrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    // 一行最多 100 字符
+    printWidth: 100,
+    // 不使用缩进符，而使用空格
+    useTabs: false,
+    // 使用 2 个空格缩进
+    tabWidth: 2,
+    tabSize: 2,
+    // 行尾需要有分号
+    semi: true,
+    // 使用单引号
+    singleQuote: true,
+    // jsx 不使用单引号，而使用双引号
+    jsxSingleQuote: false,
+    // 末尾不需要逗号 'es5'  none
+    trailingComma: 'es5',
+    // 大括号内的首尾需要空格
+    bracketSpacing: true,
+    // jsx 标签的反尖括号需要换行
+    jsxBracketSameLine: false,
+    // 在单个箭头函数参数周围加上括号<avoid|always>
+    arrowParens: 'avoid',
+    // 不需要写文件开头的 @prettier
+    requirePragma: false,
+    // 不需要自动在文件开头插入 @prettier
+    insertPragma: false,
+    // 使用默认的折行标准
+    proseWrap: 'preserve',
+    // 换行符使用 lf 结尾是 \n \r \n\r auto
+    endOfLine: 'lf',
+    vueIndentScriptAndStyle: true,
+};
+  
---
+++ commitlint.config.js
@@ -0,0 +1,37 @@
+/*
+  规范commit日志
+  https://commitlint.js.org
+*/
+
+const types = [
+    'build', // 主要目的是修改项目构建系统（例如glup，webpack，rollup的配置等）的提交
+    'ci', // 修改项目的持续集成流程（Kenkins、Travis等）的提交
+    'chore', // 构建过程或辅助工具的变化
+    'docs', // 文档提交（documents）
+    'feat', // 新增功能（feature）
+    'fix', // 修复 bug
+    'pref', // 性能、体验相关的提交
+    'refactor', // 代码重构
+    'revert', // 回滚某个更早的提交
+    'style', // 不影响程序逻辑的代码修改、主要是样式方面的优化、修改
+    'test', // 测试相关的开发,
+],
+typeEnum = {
+    rules: {
+        'type-enum': [2, 'always', types],
+    },
+    value: () => types,
+}
+
+module.exports = {
+extends: ['@commitlint/config-conventional'],
+/*
+Level [0..2]: 0 disables the rule. For 1 it will be considered a warning for 2 an error.
+https://commitlint.js.org/#/reference-rules
+*/
+rules: {
+    'type-enum': typeEnum.rules['type-enum'],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+},
+}
