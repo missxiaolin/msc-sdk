c72a7dcdc8e782848a709708d1386505eca0f3de
462441355@qq.com <462441355@qq.com>
2023-11-30 17:38:56 +0800

    * add

--- packages/sdk.js
+++ packages/sdk.js
@@ -1,11 +1,11 @@
 import HackFetch from './http/fetch.js';
-import hackXml from './http/xml.js';
+import HackXml from './http/xml.js';
 import HackWebVitals from './performance/webVitals.js';
 import HackJavascript from './error/javascript.js';
 
 export {
     HackFetch,
-    hackXml,
+    HackXml,
     HackWebVitals,
     HackJavascript
 }
