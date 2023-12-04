ffe0dd1d6097964d521b63d9752938fbfb2ac10a
462441355@qq.com <462441355@qq.com>
2023-12-04 10:01:48 +0800

    * add

--- packages/index.js
+++ packages/index.js
@@ -1,4 +1,11 @@
 import MonitorSdk from './monitor';
-
+import { loadScript, guid, monitorCookie, uaParser } from './utils/utils';
+export { guid, monitorCookie, uaParser, loadScript };
 export default MonitorSdk;
 window.MonitorSdk = MonitorSdk;
+window.MUtils = {
+  monitorCookie,
+  guid,
+  loadScript,
+  uaParser,
+};
