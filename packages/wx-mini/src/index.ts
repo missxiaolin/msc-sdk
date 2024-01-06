12a7936d83d9b09a5a4b86696ecf392ce5e0b555
lingongbing <462441355@qq.com>
2024-01-06 21:32:55 +0800

    * 初始化 微信 准备开搞微信

--- packages/wx-mini/src/index.ts
+++ packages/wx-mini/src/index.ts
@@ -1,1 +1,14 @@
-console.log(1)
+import { initOptions, log } from '../../core'
+import { isWxMiniEnv } from '../../utils'
+import { InitOptions } from '../../types'
+import { setupReplace } from './load'
+import { SDK_NAME, SDK_VERSION } from '../../shared'
+
+export function init(options: InitOptions = {}) {
+  if (!isWxMiniEnv) return
+  initOptions(options)
+  setupReplace()
+  Object.assign(wx, { mitoLog: log, SDK_NAME, SDK_VERSION })
+}
+
+export { log }
